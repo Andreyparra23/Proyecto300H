f6ee7318cf7483afd4fc1618533caed6d32ab58f
Andrey Parra <andreysebastianparrafernandez@gmail.com>
2024-11-01 22:19:28 -0500

    se agrego modelo de productos

---
+++ backend/src/models/model_licores.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const productScheme = new mongoose.Schema({
+    Image: {type: String, required: true}, //image es el nombre que nosotros le ponemos a esa "variable", caracteristicas de la info que quieren guardar
+    name: {type: String, required: true},
+    category: {type: String, required: false},
+    price: {type: Number, required: true},
+    description: {type: String}, 
+    });
+
+    export const productModel = mongoose.model('product', productScheme);
